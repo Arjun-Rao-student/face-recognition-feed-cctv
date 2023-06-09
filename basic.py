e7a73d6fa0cb6a8453934aa0afab02982916cdf2
Arjun225-star <59176699+Arjun225-star@users.noreply.github.com>
2023-06-09 11:42:37 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,9 +1,9 @@
 import face_recognition
 import cv2
 import numpy as np
-imgElon = face_recognition.load_image_file('ImagesBasic/Elon Musk.jpg')
+imgElon = face_recognition.load_image_file('ImagesBasic/mark.jpg')
 imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('ImagesBasic/Elon Test.jpg')
+imgTest = face_recognition.load_image_file('ImagesBasic/mark Test.jpg')
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 faceLoc = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
@@ -15,6 +15,6 @@ results = face_recognition.compare_faces([encodeElon], encodeTest)
 faceDis = face_recognition.face_distance([encodeElon], encodeTest)
 cv2.putText(imgTest,f'{results} {round(faceDis[0],2)} ',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),3)
 print(results,faceDis)
-cv2.imshow('Elon Musk.jpg',imgElon)
-cv2.imshow('Elon Test.jpg',imgTest)
+cv2.imshow('mark.jpg',imgElon)
+cv2.imshow('mark Test.jpg',imgTest)
 cv2.waitKey(0)
