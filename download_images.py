e6d2a1189b8f194d176bbab131ef8c1982229766
Arjun Rao <59176699+Arjun225-star@users.noreply.github.com>
2023-06-10 09:30:01 +0530

    Update and rename download_image.py to download_images.py

--- download_images.py
+++ download_images.py
@@ -1,6 +1,6 @@
 from simple_image_download import simple_image_download as simp
 response = simp.simple_image_download
-keywords = ["college staff"]
+keywords = ["mark zukar burg"] //search images
 
 for kw in keywords:
   response().download(kw,200)
